9b0c4ff4e53e10d1c0044aa788851c1ef8776e0e
Sitedept <79788120+Sitedept@users.noreply.github.com>
2024-07-17 02:03:17 +0530

    dd

--- assets/js/main.js
+++ assets/js/main.js
@@ -229,18 +229,20 @@
 })()
 
 
-function myFunction() {
+document.addEventListener("DOMContentLoaded", function() {
   var dots = document.getElementById("dots");
   var moreText = document.getElementById("more");
   var btnText = document.getElementById("myBtn");
 
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read More..."; 
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read Less"; 
-    moreText.style.display = "inline";
-  }
-}
+  btnText.onclick = function() {
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read More..."; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read Less"; 
+      moreText.style.display = "inline";
+    }
+  };
+});
